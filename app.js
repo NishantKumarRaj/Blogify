38a2504fbe1b7f653fa81943e8aaf2c3cc63e81b
NishantKumarRaj <nishantkrraj111@gmail.com>
2025-08-11 19:30:58 +0530

    Initial commit without package.json

---
+++ app.js
@@ -0,0 +1,41 @@
+require('dotenv').config()
+const path = require("path");
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const Blog=require("./models/blog")
+
+const UserRouter = require("./routes/user");
+const BlogRouter = require("./routes/blog");
+const { checkForAuthenticationCookie } = require("./middleware/authenticaton");
+
+const app = express();
+const PORT = process.env.PORT || 8001;
+
+mongoose
+  // .connect("mongodb://localhost:27017/blogify")
+  .connect(process.env.MONGO_URL)
+  .then((e) => console.log("MongoDB Connected"));
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(checkForAuthenticationCookie("token"));
+
+app.use(express.static(path.resolve("./Public")));
+
+app.get("/", async (req, res) => {
+  const allBlogs=await Blog.find({});
+  res.render("home", {
+    user: req.user,
+    blogs:allBlogs,
+  });
+});
+
+app.use("/user", UserRouter);
+app.use("/blog", BlogRouter);
+
+
+app.listen(PORT, () => console.log(`Server is Started at PORT:${PORT} `));
